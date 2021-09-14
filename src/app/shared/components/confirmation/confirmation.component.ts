7fbe3b62b1dbe9c1296b08308c39698451b31e2f
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-09-14 23:46:35 +0200

    confirmation message

---
+++ src/app/shared/components/confirmation/confirmation.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+
+export interface DialogData {
+  message: string;
+}
+
+@Component({
+  selector: 'app-confirmation',
+  templateUrl: './confirmation.component.html',
+  styleUrls: ['./confirmation.component.scss'],
+})
+export class ConfirmationComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  ngOnInit(): void {}
+}
